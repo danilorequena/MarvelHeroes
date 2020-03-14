a4cb957672733020a24ecf1771e615b3df9b4004
Danilo Requena <danilo.requena26@gmail.com>
2020-03-14 15:55:27 -0300

    Monta as telas

---
+++ MarvelSuperHeroes/HeroViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HeroViewController.swift
+//  MarvelSuperHeroes
+//
+//  Created by Danilo Requena on 14/03/20.
+//  Copyright © 2020 Danilo Requena. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HeroViewController: ViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
